f49deb5b4932964ec55524a177a2a049b74a2ce2
Lee-Scott <leehscott24@gmail.com>
2025-02-23 10:22:44 -0800

    New Project!

---
+++ ScalableWebSocketServerWithAI/src/main/java/com/familyfirstsoftware/ScalableWebSocketServerWithAI/ScalableWebSocketServerWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.familyfirstsoftware.ScalableWebSocketServerWithAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableWebSocketServerWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalableWebSocketServerWithAiApplication.class, args);
+	}
+
+}
---
+++ ScalableWebSocketServerWithAI/src/test/java/com/familyfirstsoftware/ScalableWebSocketServerWithAI/ScalableWebSocketServerWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyfirstsoftware.ScalableWebSocketServerWithAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalableWebSocketServerWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
